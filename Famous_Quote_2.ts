12de6eca0b55ed138c81de7bb7c9919bd035b5ce
akbl-student <160585930+akbl-student@users.noreply.github.com>
2024-03-07 15:15:57 +0500

    Add files via upload

---
+++ Famous_Quote_2.ts
@@ -0,0 +1,5 @@
+let famous_person :string = 'Albert Einstein';
+
+let message :string = '"A person who never made a mistake never tried anyting new.';
+
+console.log(`${famous_person} once said, ${message}`);

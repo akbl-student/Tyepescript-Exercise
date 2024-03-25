5f631aac2d4fd0f514e5cd3ebab3489fc7df91f6
akbl-student <160585930+akbl-student@users.noreply.github.com>
2024-03-25 12:45:10 +0500

    Add files via upload

---
+++ Favorite_Number.js
@@ -0,0 +1,4 @@
+"use strict";
+const Favorite_Number = 6;
+const message = 'Here is your favorite number ' + Favorite_Number;
+console.log(message);
---
+++ Favorite_Number.ts
@@ -0,0 +1,7 @@
+
+
+const Favorite_Number :number = 6;
+
+const message :string = 'Here is your favorite number ' + Favorite_Number ;
+
+console.log(message) ;
